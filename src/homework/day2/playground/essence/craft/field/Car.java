263dc12f28eac49a4d6f3ce3c5e292f541d65be8
Valeria Sapozhnikava <walerija_sap@outlook.com>
2025-04-12 01:46:13 +0200

    Task 6 is comleted

--- src/homework/day2/playground/essence/craft/field/Car.java
+++ src/homework/day2/playground/essence/craft/field/Car.java
@@ -2,6 +2,6 @@ package homework.day2.playground.essence.craft.field;
 
 public class Car extends Vehicle {
     public Car(int mass, String name) {
-        super(name, mass);
+        super(mass, name);
     }
 }
