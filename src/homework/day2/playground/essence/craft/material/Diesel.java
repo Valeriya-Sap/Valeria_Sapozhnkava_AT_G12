4468ac384cf63f6b68f38e1d6dd256aa996b2acb
Valeria Sapozhnikava <walerija_sap@outlook.com>
2025-04-18 22:58:40 +0200

    Task 3 is completed

--- src/homework/day2/playground/essence/craft/material/Diesel.java
+++ src/homework/day2/playground/essence/craft/material/Diesel.java
@@ -18,8 +18,6 @@ public class Diesel implements Pourable, Powerable {
     @Override
     public void spread(String storeName) {
         System.out.printf("I am '%s' and I am spreading in '%s'", this.getClass().getSimpleName(), storeName);
-        if (this.getClass().getSimpleName().equals(this.getClass().getSimpleName())) {
-            System.out.println("whooh..");
-        }
+        System.out.println("whooh..");
     }
 }
