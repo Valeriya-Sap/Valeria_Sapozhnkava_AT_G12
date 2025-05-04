bbe45a3e7f8e4e8145213491af6a3b518c19dcd8
Valeria Sapozhnikava <walerija_sap@outlook.com>
2025-05-04 12:54:10 +0200

    Task 5 is completed. Lists.

---
+++ src/homework/day5_Lists/MyCars.java
@@ -0,0 +1,40 @@
+package homework.day5_Lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyCars {
+    public static void main(String[] args) {
+        //Создать набор строк стрингов cars из 7 авто (Мерс, Ауди, Жигуль, Рено, Жигуль, Жигуль, Ауди) (через new ArrayList)
+
+        List<String> cars = new ArrayList<>();
+        cars.add("Мерс");
+        cars.add("Ауди");
+        cars.add("Жигуль");
+        cars.add("Рено");
+        cars.add("Жигуль");
+        cars.add("Жигуль");
+        cars.add("Ауди");
+
+        //Проитерировать список через for-each и отпечатать слова в файл cars через с новой строки в кавычках
+
+        for (String car : cars) {
+            System.out.println("\"" + car + "\"");
+        }
+
+        //Найти и удалить из набора авто, в названии которых больше 4 букв
+
+        for (int i = 0; i < cars.size(); i++) {
+            if (cars.get(i).length() > 4) {
+                cars.remove(i);
+                i--;
+            }
+        }
+
+        //Проитерировать список через for-each и отпечатать слова в консоль через пробел
+
+        for (String car : cars) {
+            System.out.print(car + " ");
+        }
+    }
+}
