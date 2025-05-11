2c2ec144937a4d55d53bedc5445d4c6f156c3b0b
Valeria Sapozhnikava <walerija_sap@outlook.com>
2025-05-11 21:28:14 +0200

    Fix

--- src/homework/day6_Stream_API/BirdsRunner.java
+++ src/homework/day6_Stream_API/BirdsRunner.java
@@ -6,16 +6,17 @@ import java.util.List;
 public class BirdsRunner {
     public static void main(String[] args) {
 //Создать список строк birds из 5 птиц (Чайка, Дрозд, Бусел, Голубь, Воробей, Цапля) через (Arrays.asList())
-        List<String> birds = Arrays.asList("Чайка", "Дрозд", "Бусел", "Голубь", "Воробей", "Цапля");
-//Создать из списка поток данных, заменить в каждую букву "о" на букву "а"
+// Создать из списка поток данных, заменить в каждую букву "о" на букву "а"
 //Собрать все слова в одну строку в ниждем регистре
 //Убрать все символы мягкого знака
 //Разбить на новые строки по букве "б"
 //Отпечатать в консоль с новой строки в виде --Чайка--
 
-        birds.stream().map(s -> s.replace("о", "а")).map(s -> s.toLowerCase()).
-                map(s -> s.replace("ь", "")).
-                flatMap(string -> Arrays.stream(string.split("б"))).
-                forEach(s -> System.out.println("--" + s + "--"));
+        Arrays.stream(Arrays.asList("Чайка", "Дрозд", "Бусел", "Голубь", "Воробей", "Цапля")
+                .stream().map(s -> s.replace("о", "а"))
+                .map(s -> s.toLowerCase())
+                .reduce("", (s1, s2) -> s1 + s2)
+                .replace("ь", "")
+                .split("б")).skip(0).forEach(s -> System.out.println("--" + s + "--"));
     }
 }
