27528df37670bf67f77eba28658125bad415c85e
Valeria Sapozhnikava <walerija_sap@outlook.com>
2025-05-11 21:43:19 +0200

    Fix

--- src/homework/day6_Stream_API/NumbersRunner.java
+++ src/homework/day6_Stream_API/NumbersRunner.java
@@ -8,7 +8,11 @@ public class NumbersRunner {
 //В потоке данных проитерировать числа и отпечатать числа в консоль новой строки
 //Отсортировать и снова отпечатать в консоль с новой строки
 //Посчитать сумму всех чисел и вывести результат в консоль
-        Stream.of(3342, 34, 79, 23426, 68, 1324, 55, 7699).peek(s -> System.out.println(s)).sorted()
-                .peek(s -> System.out.println(s)).mapToInt(s -> s.intValue()).sum();
+        System.out.println(Stream.of(3342, 34, 79, 23426, 68, 1324, 55, 7699)
+                .peek(s -> System.out.println(s))
+                .sorted()
+                .peek(s -> System.out.println(s))
+                .mapToInt(s -> s.intValue())
+                .sum());
     }
 }
