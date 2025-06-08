d2e10061b99d8e8f191b299f71d2ff5382b7003b
Valeria Sapozhnikava <walerija_sap@outlook.com>
2025-06-08 16:41:21 +0200

    Completed weather Minsk. Begining Booking.

--- src/test/java/day16_27_may/WeatherMinsk.java
+++ src/test/java/day16_27_may/WeatherMinsk.java
@@ -3,16 +3,24 @@ package day16_27_may;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WeatherMinsk {
     public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://google.com");
         //driver.findElement(By.xpath("//div[text()='Принять все']/..")).click();
-        driver.findElement(By.name ("q")).sendKeys("погода минск");
+        driver.findElement(By.name("q")).sendKeys("погода минск");
         Thread.sleep(500L);
         driver.findElement(By.xpath("//ul[@role='listbox']/li[1]")).click();
-//найти следующий день
-        driver.findElement(By.xpath("//*[@aria-label='21°Celsius среда 12:00']"));
+        //найти следующий день
+        String tomorrow = DayOfWeekMyVersion.getDayOfWeekNextYear(0);
+        String weatherTomorrow;
+        weatherTomorrow = driver.findElement(By.xpath("//*[contains(@aria-label, 'Celsius " + tomorrow + " 12:00')][1]"))
+                .getAttribute("aria-label");
+        System.out.println(weatherTomorrow);
+        //driver.findElement(By.cssSelector("text[aria-label*='Celsius среда 12:00']")
     }
 }
